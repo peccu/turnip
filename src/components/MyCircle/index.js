33168fd889f351fb16b10cef96cc2fd194762cd5
shimatani <shimatani@tgl.co.jp>
2020-06-09 23:06:27 +0900

    update null circle

--- src/components/MyCircle/index.js
+++ src/components/MyCircle/index.js
@@ -1,6 +1,19 @@
  export default {
    props: [
+     'cx',
+     'cy',
+     'r',
+     'stroke',
+     'strokeWidth',
      'fill'
    ],
-   template: `<svg width="10px" height="10px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="50" :fill="fill"/></svg>`
+   template: `<svg width="10px" height="10px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <circle
+    :cx="cx || 50"
+    :cy="cy || 50"
+    :r="r || 50"
+    :stroke="stroke"
+    :stroke-width="strokeWidth"
+    :fill="fill"/>
+</svg>`
  }
